0a69fb4df01c167647d8fec87a10f85c3c40c4b2
vivobook <maximilianolutz@hotmail.com>
2023-04-20 23:39:29 +0200

    inicio citas-Service

---
+++ servicio-citas/src/main/java/com/es/mlutzdev/agenda/citas/ServicioCitasApplication.java
@@ -0,0 +1,13 @@
+package com.es.mlutzdev.agenda.citas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioCitasApplication.class, args);
+	}
+
+}
